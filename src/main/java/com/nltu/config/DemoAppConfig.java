995dc7481f911097fce54ab01358b719f9457546
Yurii Midianyi <loopfordev@gmail.com>
2023-05-03 23:58:18 +0300

    added css suport for project

--- src/main/java/com/nltu/config/DemoAppConfig.java
+++ src/main/java/com/nltu/config/DemoAppConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.nltu")
-public class DemoAppConfig{
+public class DemoAppConfig implements WebMvcConfigurer{
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -21,5 +23,12 @@ public class DemoAppConfig{
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	
+	
+	@Override
+	//adding support for resources(css styles)
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/theme1/");
+    }
 		
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,10 +1,12 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE>
 <html>
 <head>
+	<link href="<c:url value="/resources/css/main.css" />" rel="stylesheet">
 	<title>Home page</title>
 </head>
 <body>
 <p>Success</p>
-
+<h1>1. Test CSS</h1>
 </body>
 </html>
