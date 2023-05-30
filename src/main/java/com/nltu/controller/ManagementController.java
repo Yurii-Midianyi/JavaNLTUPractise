d9eab651c87ccf50d0714eb7bff80d7688d676e1
Yurii Midianyi <loopfordev@gmail.com>
2023-05-30 15:08:26 +0300

    fixed a bug with LAZY loading for countries in management page

--- src/main/java/com/nltu/controller/ManagementController.java
+++ src/main/java/com/nltu/controller/ManagementController.java
@@ -1,18 +1,14 @@
 package com.nltu.controller;
-
-import com.nltu.entity.Country;
-import com.nltu.entity.Hotel;
-import com.nltu.entity.Room;
 import com.nltu.service.CountryService;
 import com.nltu.service.HotelService;
 import com.nltu.service.RoomService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/management")
@@ -30,7 +26,11 @@ public class ManagementController {
     }
 
     @GetMapping
+    @Transactional
     public String showManagementPage(Model model) {
+    	
+    	countryService.getCountries().toString(); //resolving issue with LAZY loading
+    	
         model.addAttribute("countries", countryService.getCountries());
 
         model.addAttribute("hotels", hotelService.getHotels());
